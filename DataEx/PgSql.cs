8a2c0c1417dcaa382dbd639b9039a3615dd0acd2
Adam Łyskawa <adam@codedog.pl>
2018-06-17 12:30:35 +0200

    Add project files.

---
+++ DataEx/PgSql.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace Woof.DataEx {
+
+    /// <summary>
+    /// Minimalistic PostgreSQL backend.
+    /// </summary>
+    class PgSql : IDbSource {
+
+        #region Configuration
+
+        /// <summary>
+        /// Connection string for PostgreSQL database access.
+        /// </summary>
+        private readonly string ConnectionString;
+
+        public PgSql(string connectionString) => ConnectionString = connectionString;
+
+        /// <summary>
+        /// Creates new PostgreSQL input parameter.
+        /// </summary>
+        /// <param name="name">Parameter name.</param>
+        /// <param name="value">Parameter value.</param>
+        /// <returns><see cref="NpgsqlParameter"/>.</returns>
+        public DbParameter I(string name, object value) => new NpgsqlParameter(name, value);
+
+        /// <summary>
+        /// Creates new PostgreSQL input / output parameter.
+        /// </summary>
+        /// <param name="name">Parameter name.</param>
+        /// <param name="value">Parameter value.</param>
+        /// <returns><see cref="NpgsqlParameter"/>.</returns>
+        public DbParameter IO(string name, object value) => new NpgsqlParameter(name, value) { Direction = ParameterDirection.InputOutput };
+
+        /// <summary>
+        /// Creates new PostgreSQL output parameter.
+        /// </summary>
+        /// <param name="name">Parameter name.</param>
+        /// <returns><see cref="NpgsqlParameter"/>.</returns>
+        public DbParameter O(string name) => new NpgsqlParameter(name, null) { Direction = ParameterDirection.Output };
+
+        
+
+        #endregion
+
+        #region Command builder
+
+        /// <summary>
+        /// Creates new <see cref="NpgsqlCommand"/> from stored procedure name and optional parameters and opens a connection if needed.
+        /// </summary>
+        /// <param name="connection">Disposable connection.</param>
+        /// <param name="procedure">Stored procedure name.</param>
+        /// <param name="parameters">Optional PostgreSQL parameters.</param>
+        /// <returns>Command.</returns>
+        private async Task<NpgsqlCommand> GetCommandAsync(NpgsqlConnection connection, string procedure, params DbParameter[] parameters) {
+            var command = new NpgsqlCommand(procedure, connection) { CommandType = CommandType.StoredProcedure };
+            if (parameters.Length > 0) foreach (var parameter in parameters) command.Parameters.Add(parameter);
+            if (connection.State != ConnectionState.Open) await connection.OpenAsync();
+            return command;
+        }
+
+        #endregion
+
+        #region Data operations
+
+        /// <summary>
+        /// Executes a stored procedure asynchronously and returns number of rows affected.
+        /// </summary>
+        /// <param name="procedure">Stored procedure name.</param>
+        /// <param name="parameters">Optional <see cref="NpgsqlParameter"/> parameters.</param>
+        /// <returns>-1, due to the driver lack of correct implementation.</returns>
+        public async Task<int> ExecuteAsync(string procedure, params DbParameter[] parameters) {
+            using (var connection = new NpgsqlConnection(ConnectionString))
+            using (var command = await GetCommandAsync(connection, procedure, parameters)) return await command.ExecuteNonQueryAsync();
+        }
+
+        /// <summary>
+        /// Executes a stored procedure asynchronously and returns a scalar.
+        /// </summary>
+        /// <param name="procedure">Stored procedure name.</param>
+        /// <param name="parameters">Optional <see cref="NpgsqlParameter"/> parameters.</param>
+        /// <returns>Scalar object.</returns>
+        public async Task<T> GetScalarAsync<T>(string procedure, params DbParameter[] parameters) {
+            object value = null;
+            using (var connection = new NpgsqlConnection(ConnectionString))
+            using (var command = await GetCommandAsync(connection, procedure, parameters)) value = await command.ExecuteScalarAsync();
+            if (value == null || value is DBNull) return default(T);
+            else return (T)value;
+        }
+
+        /// <summary>
+        /// Executes a stored procedure asynchronously and returns a dataset.
+        /// </summary>
+        /// <param name="procedure">Stored procedure name.</param>
+        /// <param name="parameters">Optional <see cref="NpgsqlParameter"/> parameters.</param>
+        /// <returns>Table data.</returns>
+        public async Task<object[][]> GetTableAsync(string procedure, params DbParameter[] parameters) {
+            var table = new List<object[]>();
+            using (var connection = new NpgsqlConnection(ConnectionString))
+            using (var command = await GetCommandAsync(connection, procedure, parameters)) {
+                using (var reader = await command.ExecuteReaderAsync()) {
+                    while (await reader.ReadAsync()) {
+                        object[] values = new object[reader.FieldCount];
+                        reader.GetValues(values);
+                        table.Add(values);
+                    }
+                }
+            }
+            return table.ToArray();
+        }
+
+        /// <summary>
+        /// Executes a stored procedure asynchronously and returns a dataset.
+        /// </summary>
+        /// <typeparam name="T">Record type.</typeparam>
+        /// <param name="procedure">Stored procedure name.</param>
+        /// <param name="parameters">Optional <see cref="NpgsqlParameter"/> parameters.</param>
+        /// <returns>Table data.</returns>
+        public async Task<T[]> GetTableAsync<T>(string procedure, params DbParameter[] parameters) where T : new()
+            => (await GetTableAsync(procedure, parameters)).AsArrayOf<T>();
+
+        /// <summary>
+        /// Executes a stored procedure asynchronously and returns a record.
+        /// </summary>
+        /// <typeparam name="T">Record type.</typeparam>
+        /// <param name="procedure">Stored procedure name.</param>
+        /// <param name="parameters">Optional <see cref="NpgsqlParameter"/> parameters.</param>
+        /// <returns>Table data.</returns>
+        public async Task<T> GetRecordAsync<T>(string procedure, params DbParameter[] parameters) where T : new() {
+            var raw = await GetTableAsync(procedure, parameters);
+            var row = raw?.FirstOrDefault();
+            return row.As<T>();
+        }
+
+        /// <summary>
+        /// Executes a stored procedure asynchronously and returns multiple datasets.
+        /// </summary>
+        /// <param name="procedure">Stored procedure name.</param>
+        /// <param name="parameters">Optional <see cref="NpgsqlParameter"/> parameters.</param>
+        /// <returns>An array of table data.</returns>
+        public async Task<object[][][]> GetDataAsync(string procedure, params DbParameter[] parameters) {
+            var data = new List<object[][]>();
+            var table = new List<object[]>();
+            using (var connection = new NpgsqlConnection(ConnectionString))
+            using (var command = await GetCommandAsync(connection, procedure, parameters)) {
+                using (var reader = await command.ExecuteReaderAsync()) {
+                    do {
+                        while (await reader.ReadAsync()) {
+                            object[] values = new object[reader.FieldCount];
+                            reader.GetValues(values);
+                            table.Add(values);
+                        }
+                        data.Add(table.ToArray());
+                        table.Clear();
+                    } while (await reader.NextResultAsync());
+                }
+            }
+            return data.ToArray();
+        }
+
+        
+
+        #endregion
+
+    }
+
+}
---
+++ DeploymentEx/PostgresClientInstaller.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using Woof.SystemEx;
+
+
+namespace Woof.DeploymentEx {
+
+    /// <summary>
+    /// Automatic PostgreSQL client installer.
+    /// </summary>
+    public class PostgresClientInstaller : IDisposable {
+
+        #region Configuration
+
+        static readonly string BinariesDownloadURL = "https://www.enterprisedb.com/download-postgresql-binaries";
+        static readonly string WindowsX64RelPattern =
+            Target.IsX86
+                ? "/postgresql-*-binaries-win32"
+                : "/postgresql-*-binaries-win64";
+        static readonly string ZipArchivePattern =
+            Target.IsX86
+                ? "https://get.enterprisedb.com/postgresql/postgresql-*-windows-binaries.zip"
+                : "https://get.enterprisedb.com/postgresql/postgresql-*-windows-x64-binaries.zip";
+        static readonly string BinDirectory = "pgsql\\bin";
+        static readonly string[] ClientFiles = new string[] {
+            "iconv.dll", "libeay32.dll", "libiconv-2.dll", "libintl-8.dll", "libpq.dll", "msvcr120.dll", "ssleay32.dll", "zlib1.dll",
+            "pg_dump.exe", "pg_dumpall.exe", "pg_restore.exe", "psql.exe"
+        };
+        static readonly string TargetPath = Path.Combine(Target.GetProgramFilesDirectory(Target.Auto), "PGSQL");
+        
+        private readonly TextWriter MessageOutput;
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Occurs when the download link is found.
+        /// </summary>
+        public event EventHandler DownloadLinkFound;
+
+        /// <summary>
+        /// Occurs when the download process is started.
+        /// </summary>
+        public event EventHandler DownloadStarted;
+
+        /// <summary>
+        /// Occurs when the download progress advanced by at least 1%.
+        /// </summary>
+        public event EventHandler<PercentEventArgs> DownloadProgressChanged;
+
+        /// <summary>
+        /// Occurs when the download has completed.
+        /// </summary>
+        public event EventHandler DownloadCompleted;
+
+        /// <summary>
+        /// Occurs when a file extraction is started.
+        /// </summary>
+        public event EventHandler<ItemEventArgs<ZipArchiveEntry>> ExtractingFile;
+
+        /// <summary>
+        /// Occurs when a file extraction is completed.
+        /// </summary>
+        public event EventHandler ExtractingFileDone;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets a value indicating whether PostgreSQL client is installed.
+        /// </summary>
+        public static bool IsInstalled => PathTools.IsFileAcessibleInPath("psql.exe");
+
+        /// <summary>
+        /// Gets a PostgreSQL client binaries directory if available.
+        /// </summary>
+        public static string Bin => IsInstalled ? Path.GetDirectoryName(PathTools.GetFullPath("psql.exe")) : null;
+
+        #endregion
+
+        /// <summary>
+        /// Installs the latest version of PostgreSQL client binaries
+        /// from the EDB website to user or system program files directory
+        /// and adds it to the environment path.
+        /// Invoke as user to make per-user installation, invoke as Administrator to make per-machine installation.
+        /// Compile with "Prefer 32-bit" to install 32-bit version, uncheck "Prefer 32-bit" to install x64 version.
+        /// Blocks current thread until completed.
+        /// </summary>
+        /// <param name="messageOutput">
+        /// Optional text writer to write diagnostic messages to, like <see cref="Console.Out"/>.
+        /// Do not provide if you intend to use events instead.
+        /// </param>
+        /// <returns>True if successfull.</returns>
+        public static bool Install(TextWriter messageOutput = null) {
+            using (var installer = new PostgresClientInstaller(messageOutput)) return installer.Install();
+        }
+
+        /// <summary>
+        /// Ensures the PostgreSQL client binaries are installed.
+        /// If the aren't they will be.
+        /// </summary>
+        /// <param name="messageOutput">
+        /// Optional text writer to write diagnostic messages to, like <see cref="Console.Out"/>.
+        /// Do not provide if you intend to use events instead.
+        /// </param>
+        /// <returns>True if PostgreSQL client binaries are installed correctly.</returns>
+        public static bool EnsureInstalled(TextWriter messageOutput = null) {
+            if (PathTools.IsFileAcessibleInPath("psql.exe")) return true;
+            using (var installer = new PostgresClientInstaller(messageOutput)) return installer.Install();
+        }
+
+        /// <summary>
+        /// Creates new PostgreSQL installer with optional message output.
+        /// </summary>
+        /// <param name="messageOutput">
+        /// Optional text writer to write diagnostic messages to, like <see cref="Console.Out"/>.
+        /// Do not provide if you intend to use events instead.
+        /// </param>
+        public PostgresClientInstaller(TextWriter messageOutput = null) => MessageOutput = messageOutput;
+
+        /// <summary>
+        /// Installs the latest version of PostgreSQL client binaries
+        /// from the EDB website to user or system program files directory
+        /// and adds it to the environment path.
+        /// Invoke as user to make per-user installation, invoke as Administrator to make per-machine installation.
+        /// Compile with "Prefer 32-bit" to install 32-bit version, uncheck "Prefer 32-bit" to install x64 version.
+        /// Blocks current thread until completed.
+        /// </summary>
+        /// <returns>True if successfull.</returns>
+        public bool Install() {
+            Directory.CreateDirectory(TargetPath);
+            if (Download(TargetPath)) {
+                PathTools.AddToPath(TargetPath, Target.Auto);
+                return true;
+            }
+            else return false;
+        }
+
+        /// <summary>
+        /// Downloads and extracts the latest Windows client binaries.
+        /// Blocks the current thread until completed.
+        /// </summary>
+        /// <param name="targetPath"></param>
+        /// <returns></returns>
+        private bool Download(string targetPath) {
+            var t0 = DateTime.Now;
+            MessageOutput?.WriteLine("Starting PostgeSQL client installer...");
+            var edb = new Uri(BinariesDownloadURL);
+            var link = LinkTool.FetchLastVersionLink(edb, WindowsX64RelPattern, ZipArchivePattern);
+            if (link == null) {
+                MessageOutput?.WriteLine("Download error.");
+                return false;
+            }
+            if (MessageOutput != null) {
+                DownloadLinkFound += (s, e) => MessageOutput.WriteLine($"Download link found...");
+                DownloadProgressChanged += (s, e) => MessageOutput.Write(".");
+                DownloadStarted += (s, e) => MessageOutput.WriteLine($"Downloading {link}...");
+                ExtractingFile += (s, e) => MessageOutput.Write($"Extracting file {e.Item.Name}...");
+                ExtractingFileDone += (s, e) => MessageOutput.WriteLine("OK.");
+                DownloadCompleted += (s, e) => MessageOutput.WriteLine();
+            }
+            DownloadLinkFound?.Invoke(link, EventArgs.Empty);
+            using (var download = new Download(link)) {
+                DownloadStarted?.Invoke(this, EventArgs.Empty);
+                download.DownloadCompleted += (s, e) => DownloadCompleted?.Invoke(this, EventArgs.Empty);
+                download.DownloadProgressChanged += (s, e) => DownloadProgressChanged?.Invoke(this, e);
+                var stream = download.GetStream();
+                if (stream == null) return false;
+                using (var archive = new ZipArchive(stream, ZipArchiveMode.Read)) {
+                    foreach (var entry in archive.Entries.Where(i => Path.GetDirectoryName(i.FullName) == BinDirectory && ClientFiles.Contains(i.Name))) {
+                        ExtractingFile?.Invoke(archive, new ItemEventArgs<ZipArchiveEntry>(entry));
+                        entry.ExtractToFile(Path.Combine(targetPath, entry.Name), true);
+                        ExtractingFileDone?.Invoke(archive, EventArgs.Empty);
+                    }
+                }
+            }
+            var t = DateTime.Now - t0;
+            MessageOutput?.WriteLine($"Completed in {t.TotalSeconds:0.000}s.");
+            return true;
+        }
+
+        /// <summary>
+        /// Releases memory allocated by the download.
+        /// </summary>
+        public void Dispose() => GC.Collect();
+
+    }
+
+
+}
